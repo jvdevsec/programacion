b58e63376f898788eb58368f16151553feeb58ac
aOne1or-aZer0 <asahi_010101@protonmail.com>
2024-10-13 19:38:35 -0500

    Arreglos bidimensionales

--- FundamentosDeProgramación/Unidad2-EstructurasRepetitivasyArreglos/Arreglos/Program.cs
+++ FundamentosDeProgramación/Unidad2-EstructurasRepetitivasyArreglos/Arreglos/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Nombre: Juan Esteban Valdés Rodriguez
+Grupo: 213022A_1704
+Programa: Ingeniería de Sistemas
+Código Fuente: Autoría Propia
+
+Problema #3 Areglos Bidimensionales:
+Se solicita la creación de una aplicación que elabore una matriz de 4 × 4 
+en la cual se almacenarán números entre el 3 y el 6 ambos incluidos que se deben capturar por teclado. 
+
+Una vez obtenida la información se debe establecer cuantas veces se repiten cada uno de los números del 3 al 6 
+y hallar la potencia de ellos de acuerdo con las veces que se repita. 
+
+Se debe mostrar por pantalla la matriz generada y las potencias de los 4 números.
+*/
+using System;
+
+namespace ArreglosBidimiensonales
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Matriz 4x4
+            int [,] numeros = new int[4,4];
+            Console.WriteLine(numeros.Length);
+            Console.WriteLine("Ingresa los numeros para la matriz");
+
+            // Bucles anidados para ingresar los 
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    while(true)
+                    {
+                        Console.WriteLine($"Ingrese un valor para la fila {x+1} y la columna {y+1}: ");
+                        string? entradaUsuario = Console.ReadLine();
+                        if (int.TryParse(entradaUsuario, out int numero) && numero >= 3 && numero <= 6) // Validar la entrada del usuario - 
+                        {
+                            numeros[x,y] = numero;
+                            break;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor=ConsoleColor.Red;
+                            Console.WriteLine("Error - Se debe ingresar un valor entre 3 y 6. Tampoco se permiten caracteres ni cadenas de texto");
+                            Console.ResetColor();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Matriz 4x4:");
+            // Mostrar en la consola el arreglo - Se usan bucles for anidados para recorrer los elementos del arreglo bidimensional
+            for(int x = 0; x < 4; x++)
+            {
+                for(int y = 0; y < 4; y++)
+                {
+                    Console.ForegroundColor=ConsoleColor.DarkGreen;
+                    Console.Write(numeros[x,y] + " ");
+                    Console.ResetColor();
+                }
+            Console.WriteLine(); // Salto de linea
+            }
+        }
+    }
+}
