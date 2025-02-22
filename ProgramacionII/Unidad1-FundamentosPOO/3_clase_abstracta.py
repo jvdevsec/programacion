12bc78dc5f2d8ca21eca92e213e66dc1aa3b675d
jvdevsec <asahi_010101@protonmail.com>
2025-02-22 01:16:58 -0500

    update: comentario añadido y argumentos cambiamos para el calculo

--- ProgramacionII/Unidad1-FundamentosPOO/3_clase_abstracta.py
+++ ProgramacionII/Unidad1-FundamentosPOO/3_clase_abstracta.py
@@ -21,15 +21,27 @@ class Gerente(Empleado):
         return  self.valor_hora * self.numero_horas
 
 class Vendedor:
-      def __init__(self, numero_horas, valor_hora): # Constructor y atributos
+      def __init__(self, numero_horas, valor_hora): # 
         self.numero_horas = numero_horas
         self.valor_hora = valor_hora
       def calcular_salario(self):
           return  self.valor_hora * self.numero_horas
       
+
+"""
+horas trabajadas a la semana: 46
+46/6 = 7.66 horas al dia
+7.66 x 30 = 230 horas al mes
+
+valor hora de trabajo 2025: $6.189
+
+Para el caso del gerente investigue el promedio del salario
+salario gerente: $5.054.040
+5.054.040/230 = 21974 x hora 
+"""
 # Instancias de los objetos vendedor y gerente - Simplemente se agregan los argumentos para el calculo de los salarios
-vendedor = Vendedor(6189, 230) # valor de la hora y el numero de horas
-gerente = Gerente (27083, 230)
+vendedor = Vendedor(6189, 230) 
+gerente = Gerente (21974, 230)
 
 print("---- Calculadora Salarios ----")
 print(f"Salario total vendedor: ${vendedor.calcular_salario()}")
