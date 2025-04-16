42459fc4c56159bb351dac68ee8af011a4d64e44
jvdevsec <asahi_010101@protonmail.com>
2025-04-16 18:33:41 -0500

    feat: subclase triangulo añadida con atributos encapsulados y implementaciones propias de los metodos

--- ProgramacionII/Unidad2-HerenciaPolimorfismoSobrecarga/6_clase_forma.py
+++ ProgramacionII/Unidad2-HerenciaPolimorfismoSobrecarga/6_clase_forma.py
@@ -8,17 +8,23 @@ para el número de lados y la longitud de cada lado.
 
 
 class Forma():
-    def __init__(self, numero_lados, longitud):
-        self.numero_lados = numero_lados
-        self.longitud = longitud
+    def __init__(self):
+        pass
 
     def area(self):
-        print(0)
+        return 0
 
     def perimetro(self):
-        print(0)
+        return 0
 
 
 class Triangulo(Forma):
-    def __init__(self):
-        pass
+    def __init__(self, numero_lados, longitud):
+        self.__numero_lados = numero_lados
+        self.__longitud = longitud
+
+    def area(self, base, altura):
+        return base * altura
+
+    def perimetro(self):
+        return self.__numero_lados * self.__longitud
