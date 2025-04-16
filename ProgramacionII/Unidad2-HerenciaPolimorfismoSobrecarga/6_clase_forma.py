a1d6cbbb6d4a2eff918a37b9eed2b05ef2267932
jvdevsec <asahi_010101@protonmail.com>
2025-04-16 17:18:48 -0500

    feat: ejercicio 6 añadido

---
+++ ProgramacionII/Unidad2-HerenciaPolimorfismoSobrecarga/6_clase_forma.py
@@ -0,0 +1,18 @@
+"""
+EJercicio 6:
+Define una clase Forma con métodos area() y perimetro() como 0.
+Crea subclases Triangulo y Pentagono, cada una con su propia
+implementación de estos métodos. Añade atributos encapsulados
+para el número de lados y la longitud de cada lado.
+"""
+
+
+class Forma():
+    def __init__(self):
+        pass
+
+    def area(self):
+        print(0)
+
+    def perimetro(self):
+        print(0)
