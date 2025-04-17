77c75de85b9feb03dc49d6f71d2e35cb9c969570
jvdevsec <asahi_010101@protonmail.com>
2025-04-16 20:36:49 -0500

    feat: se separaron las clases y la logica de ejecucion. Objetos creados

--- ProgramacionII/Unidad2-HerenciaPolimorfismoSobrecarga/6_clase_forma.py
+++ ProgramacionII/Unidad2-HerenciaPolimorfismoSobrecarga/6_clase_forma.py
@@ -58,11 +58,26 @@ class Pentagono(Forma):
     def perimetro(self):
         """
         Metodo para el calculo del perimetro con implementacion
-        propia de la clase Pentagono.
+        propia de la subclase Pentagono.
         """
         # Perimetro -> 5 * longitud
         return self.__numero_lados * self.__longitud_lados
 
+    def area(self):
+        """
+        La apotema es una medida necesaria para el calculo de
+        diferentes propiedades de los poligonos.
+        Se invoca el metodo perimetro para el calculo del area.
+        """
+        # Formula para el area = (perimetro * apotema) / 2
+        return (self.perimetro() * self.__apotema) / 2
 
+
+# Ejecucion del programa
 if __name__ == "__main__":
     print("-- Cálculo del Perímetro y Área de Formas --")
+    # Instancias de las subclases - objetos
+    triangulo = Triangulo(longitud_lados=6.0, base=3.0, altura=5.0)
+    pentagono = Pentagono(longitud_lados=8.0, apotema=4.0)
+    print(f"<<Triangulo>> Area: {triangulo.area()} Perimetro: {triangulo.perimetro()}")
+    print(f"<<Pentagono>> Area: {pentagono.area()} Perimetro: {pentagono.perimetro()}")
