20503c9875763ffeaff10d0e6193af5e0f0cc3a2
aOne1or-aZer0 <asahi_010101@protonmail.com>
2025-05-13 08:51:42 -0500

    feat: clase GeneradorContraseña() y condición __main__

--- ProgramacionII/Unidad4-PresentacionResultados/generador_contraseñas.py
+++ ProgramacionII/Unidad4-PresentacionResultados/generador_contraseñas.py
@@ -1,6 +1,9 @@
 """
 Generador de contraseñas aleatorias
 
+Estudiante: Juan Esteban Valdés Rodriguez
+Curso: Programación
+
 • La contraseña debe tener una longitud especificada por el usuario
 (mínimo 8 caracteres).
 • Debe incluir al menos un número, una letra mayúscula, una letra
@@ -9,5 +12,18 @@ minúscula y un carácter especial de la siguiente lista: ¿¡?=)(/¨*+-
 • No se permiten datos repetidos en la contraseña.
 • La asignación de los datos debe ser completamente aleatoria, es
 decir, sin un orden específico.
-
 """
+# Modulos necesarios para generar las contraseñas aleatorias
+
+import secrets
+import random
+
+class GeneradorContraseña():
+    def __init__(self, longitud, password):
+        self.longitud = longitud
+        self.password = password
+
+if __name__ == "__main__":
+    print("Testing...")
+
+
