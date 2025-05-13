4c0dff90e58970fb74ffd888b0afa7cc4a7e1d01
aOne1or-aZer0 <asahi_010101@protonmail.com>
2025-05-13 08:40:28 -0500

    doc: comentario explicando el fin del programa/script

---
+++ ProgramacionII/Unidad4-PresentacionResultados/generador_contraseñas.py
@@ -0,0 +1,13 @@
+"""
+Generador de contraseñas aleatorias
+
+• La contraseña debe tener una longitud especificada por el usuario
+(mínimo 8 caracteres).
+• Debe incluir al menos un número, una letra mayúscula, una letra
+minúscula y un carácter especial de la siguiente lista: ¿¡?=)(/¨*+-
+%&$#!.
+• No se permiten datos repetidos en la contraseña.
+• La asignación de los datos debe ser completamente aleatoria, es
+decir, sin un orden específico.
+
+"""
